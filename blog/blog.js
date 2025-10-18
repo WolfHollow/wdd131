06014e7bde9848c2220bb66dddf9d794d904dd13
Elijah Oliphant <elephant2go@gmail.com>
2025-10-18 17:36:29 -0600

    w05

---
+++ blog/blog.js
@@ -0,0 +1,57 @@
+const articles = [
+	{
+		id: 1,
+		title: 'Septimus Heap Book One: Magyk',
+		date: 'July 5, 2022',
+		description:
+			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+		imgAlt: 'Book cover for Septimus Heap 1',
+		ages: '10-14',
+		genre: 'Fantasy',
+		stars: '****'
+	},
+	{
+		id: 2,
+		title: 'Magnus Chase Book One: Sword of Summer',
+		date: 'December 12, 2021',
+		description:
+			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		imgSrc:
+			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+		imgAlt: 'Book cover for Magnus Chase 1',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	}
+]
+
+// 1. Get the container element
+const container = document.getElementById('articles');
+
+// 2. Use map() to create an array of HTML strings
+const articleHTMLArray = articles.map(article => {
+    // Template literal (backticks) for easy string formatting
+    return `
+        <article class="article-card" data-id="${article.id}">
+			<div class="book">
+				<h2>${article.title}</h2>
+            	<img src="${article.imgSrc}" alt="${article.imgAlt}">
+            	<p>${article.description}</p>
+			</div>
+            <ul class="details">
+				<li> ${article.date}</li>
+                <li> ${article.ages}</li>
+                <li> ${article.genre}</li>
+                <li> ${article.stars}</li>
+            </ul>
+        </article>
+        <hr>
+    `;
+});
+
+// 3. Use join('') to combine the array of strings into one big string
+const finalHTML = articleHTMLArray.join('');
+
+// 4. Inject the single string into the HTML container
+container.innerHTML = finalHTML;
